79df86e0a5c3fe2ec6ce85ef3828b3e7835cba73
Morgan Hartman <littlemiss.hartman@gmail.com>
2019-03-07 23:30:21 -0700

    addAPI

--- game.js
+++ game.js
@@ -6,6 +6,9 @@ let app = new Vue({
 		loginType: "email",
 		loginInput: "",
 		username: "",
+		email: "",
+		emailMessage: "",
+		showEmailMessage: false,
 		turns: 0,
 		timer: null,
 		time: "--:--",
@@ -22,17 +25,52 @@ let app = new Vue({
 		},
 		closeLogin: function() {
 			this.showLogin = false;
+			this.emailMessage = "";
+			this.showEmailMessage = false;
 		},
 		openLogin: function() {
 			this.showLogin = true;
 		},
 		handleInput: function() {
-			console.log(this.loginInput);
-			if(this.loginType === "username") this.username = this.loginInput;
-			if (this.username === "morgan_hartman") {
-				this.loggedIn = true;
-				this.showLogin = false;
+			if(this.loginType === "username") {
+				this.username = this.loginInput;
+				if (this.username === "morgan_hartman") {
+					this.loggedIn = true;
+					this.closeLogin();
+				}
 			}
+			else if (this.loginType === "email") {
+				var _this = this;
+				this.email = this.loginInput;
+        		console.log(`Email Input is ${this.email}.`);
+        		const accessKey = "81a42f979a60a5020baa7b1ec7c058e5";
+        		const url = "http://apilayer.net/api/check?access_key=" + accessKey + "&email=" + this.email;
+        		fetch(url)
+            		.then(function(response) {
+                		return response.json();
+            		}).then(function(json) {
+                		console.log(json);
+                		if (json.score >= 0.8 || json.format_valid === true) {
+							console.log("success");
+							_this.emailMessage = "Valid";
+							_this.username = _this.email;
+							_this.loggedIn = true;
+                		} else {
+							console.log("fail");
+							_this.emailMessage = "Please enter a valid email address.";
+							_this.loggedIn = false;
+                		}
+					});
+				this.showEmailMessage = true;
+			} else {
+				console.log('error');
+				this.emailMessage = "Please enter a valid email address or username."
+				this.loggedIn = false;
+				this.showEmailMessage = true;
+			}
+
+			//REDO THIS. Needs to be await
+			if (this.loggedIn) this.closeLogin();
 		},
 		logOut: function() {
 			this.loggedIn = false;
